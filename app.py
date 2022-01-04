82c1b60f553366314330f40bfeffcc704a3bced1
Crafty <craftyrite2011@gmail.com>
2022-01-04 11:28:35 +0530

    Beuty 2

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('base.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
